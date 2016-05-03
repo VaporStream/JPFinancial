90f3e013ecd0db2a9ff2ed468aee72f2f4ba7311
pars6441 <ryanp6441@gmail.com>
2016-05-02 18:13:06 -0600

    initial commit before major changes

---
+++ sqlScripts/dbo.Company.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(75) NOT NULL, 
+    [Ticker] NCHAR(10) NOT NULL, 
+    [SectorId] INT NOT NULL, 
+    [IndustryId] INT NOT NULL
+)
